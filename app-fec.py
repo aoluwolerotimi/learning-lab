a751c1f05df680704ee5848b9b4577186bcb380a
aoluwolerotimi <a.oluwolerotimi@gmail.com>
2024-11-25 14:45:52 -0500

    need to fix dependent form logic

--- app-fec.py
+++ app-fec.py
@@ -3,15 +3,37 @@ import pandas as pd
 import plotly.express as px
 
 # network url http://10.0.0.61:8501 
-# may not work well in safari, chrome recommended 
-# hmm, url not secure. may not be able to be opened at work. maybe example flow in ppt?
+# until i deploy
+
+l_submit = False
+def launch_loans():
+    st.subheader("Time to make your loan allocations")
+    loan_form = st.form('u_loans')
+    c1_loan = loan_form.slider(label= f"Please select your allocation for {c_names[0]}. A minimum of $25M must be allocated",
+                                min_value = 25,
+                                max_value = 425,
+                                step = 25,
+                                key = 'c1')
+    c2_loan = loan_form.slider(label= f"Please select your allocation for {c_names[1]}. A minimum of $25M must be allocated",
+                                min_value = 25,
+                                max_value = 425,
+                                step = 25,
+                                key = 'c2')
+    c3_loan = loan_form.slider(label= f"Please select your allocation for {c_names[2]}. A minimum of $25M must be allocated",
+                                min_value = 25,
+                                max_value = 425,
+                                step = 25,
+                                key = 'c3')
+    c4_loan = loan_form.slider(label= f"Please select your allocation for {c_names[3]}. A minimum of $25M must be allocated",
+                                min_value = 25,
+                                max_value = 425,
+                                step = 25,
+                                key = 'c4')
+    l_submit = loan_form.form_submit_button('Submit')
 
 st.title('Financed Emissions Calculator')
 st.text('This is a web app to learn about financed emissions')
 st.markdown('### Introduction')
-
-# uploaded = st.file_uploader('Upload your file here')
-# I think i'll just load a dictionary for simplicity
 
 raw = {
     "company": ["Tesla", "Stellantis", "Subaru", "Kia", "Nissan", "Hyundai", "Honda", "BMW", "Volvo"],
@@ -23,7 +45,7 @@ raw = {
 }
 
 data = pd.DataFrame(raw)
-
+# u_selections = {}
 st.header('User Inputs')
 # start by just receiving and printint out the inputs
 comp_form = st.form("u_comp")
@@ -32,25 +54,62 @@ c_names = comp_form.multiselect(label = "Please select exactly four companies to
                       max_selections= 4,
                       key = 'c_names'
                       )
-# consider this practice, more likely that i break it out so users know exactly which company they're selecting for
-# so let's say this is company 1 allocation
-c1_a = comp_form.slider(label="Please select your allocation for this company. A minimum of $25M must be allocated",
-                        min_value=25,
-                        max_value=425,
-                        step = 25)
+# c1_loan = comp_form.slider(label=f"Please select your allocation for {c_names[0]}. A minimum of $25M must be allocated",
+#                         min_value=25,
+#                         max_value=425,
+#                         step = 25) 
+
+c_submit = comp_form.form_submit_button('Submit') 
+
+if c_submit:
+    # Check if total of 4 selected
+    total = len(set(c_names))
+    if total == 4:
+        st.success(f"You selected: {c_names}") # next iteration, print more nicely than this
+        u_selections = dict.fromkeys(c_names)
+        launch_loans()
 
 
+    else:
+        st.error(f"Number of companies selected: {total}. Please select exactly 4")
+        # c_submit = False # not sure if this makes a difference - looks like no. at least for now
 
-# uncontained multiselect
-u_cs_1 = st.multiselect(label = "Please select exactly four companies to provide loans to", 
-                      options= data['company'].unique(),
-                      max_selections= 4,
-                      key='u_cs_1'
-                      )
+if l_submit:
+    st.write(c1_loan)
+
+
+# company allocation selection based on prior company selection
+
+    
+    # run validations
+    # if passed print confirmation message with st.success
+    # if failed, print error message and set c_submit to false 
+
+# def comp_val(companies = c_names):
+#     # Check if total of 4 selected
+#     total = len(set(c_names))
+
+
+# would do 
+
+
+# # uncontained multiselect
+# u_cs_1 = st.multiselect(label = "Please select exactly four companies to provide loans to", 
+#                       options= data['company'].unique(),
+#                       max_selections= 4,
+#                       key='u_cs_1'
+#                       )
 
 # returns a list. so let's check in the list that it's 4. 
 # multiselect already enforces spelling and not duplicating entries
 
+
+
+# use this as a reference when i get to the allocation selections
+# c1_a = comp_form.slider(label="Please select your allocation for this company. A minimum of $25M must be allocated",
+#                         min_value=25,
+#                         max_value=425,
+#                         step = 25) 
 
 st.header('Descriptive Stats')
 st.write(data.describe())
